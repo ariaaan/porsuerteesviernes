c15566288a704df9caffc0aa9a1bca073c21b5a8
Arian Giles Garcia <arian2822@gmail.com>
2021-04-15 12:53:24 -0300

    Tweet app

---
+++ viernes/twitter.py
@@ -0,0 +1,20 @@
+"""Twitter."""
+import emojis
+import tweepy
+
+import settings
+
+
+VIDEO_URL = "https://www.youtube.com/watch?v=WZC5VsqKqH8&t=77s"
+
+
+def generate_tweet():
+    return f"Pero por suerte es viernes {emojis.get_random_friday_emoji()}!\n\n{VIDEO_URL}"
+
+
+def twitter_auth():
+    auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_SECRET_KEY)
+    auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    
+    return api
